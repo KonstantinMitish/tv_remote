a7f1f7434448f72edce3ea5cdc18175ba34ed2ec
Kawnstawtin Miwtiwsh~ <mitikostya14@gmail.com>
2025-06-12 14:53:40 +0300

    Code style, optimization and other shit

--- tv_remote.ino
+++ tv_remote.ino
@@ -1,41 +1,32 @@
+#include <Arduino.h>
 #include <IRremote.hpp>
 
 static constexpr uint8_t IR_PIN = 4;// PIN D2
 
-static const uint8_t pin_to_gpio[] = {
-        GPIO_NUM_16,
-        GPIO_NUM_5,
-        GPIO_NUM_4,
-        GPIO_NUM_0,
-        GPIO_NUM_2,
-        GPIO_NUM_14,
-        GPIO_NUM_12,
-        GPIO_NUM_13,
-};
-static const size_t pins_count = sizeof(pin_to_gpio) / sizeof(pin_to_gpio[0]);
+static const uint8_t UNUSED = 0xFF;
 
 static const uint8_t button_to_gpio[] = {
-        /* 0x0 */ 0xFF,
-        /* 0x1 */ pin_to_gpio[1],// SOURCE
-        /* 0x2 */ pin_to_gpio[3],// POWER
-        /* 0x3 */ 0xFF,
-        /* 0x4 */ 0xFF,
-        /* 0x5 */ 0xFF,
-        /* 0x6 */ 0xFF,
-        /* 0x7 */ pin_to_gpio[4],// VOLUME_UP
-        /* 0x8 */ 0xFF,
-        /* 0x9 */ 0xFF,
-        /* 0xA */ 0xFF,
-        /* 0xB */ pin_to_gpio[5],// VOLUME_DOWN
-        /* 0xC */ 0xFF,
-        /* 0xD */ 0xFF,
-        /* 0xE */ 0xFF,
-        /* 0xF */ 0xFF};
+        /* 0x0 */ UNUSED,
+        /* 0x1 */ 5,// SOURCE -> D1
+        /* 0x2 */ 0,// POWER -> D3
+        /* 0x3 */ UNUSED,
+        /* 0x4 */ UNUSED,
+        /* 0x5 */ UNUSED,
+        /* 0x6 */ UNUSED,
+        /* 0x7 */ 2,// VOLUME_UP -> D4
+        /* 0x8 */ UNUSED,
+        /* 0x9 */ UNUSED,
+        /* 0xA */ UNUSED,
+        /* 0xB */ 14,// VOLUME_DOWN -> D5
+        /* 0xC */ UNUSED,
+        /* 0xD */ UNUSED,
+        /* 0xE */ UNUSED,
+        /* 0xF */ UNUSED};
 static const size_t buttons_count = sizeof(button_to_gpio) / sizeof(button_to_gpio[0]);
 
 void switch_buttons(uint8_t button_mask) {
     for (int i = 0; i < buttons_count; i++) {
-        if (button_to_gpio[i] == 0xFF) {
+        if (button_to_gpio[i] == UNUSED) {
             continue;
         }
         if (button_mask & (1 << i)) {
@@ -48,15 +39,14 @@ void switch_buttons(uint8_t button_mask) {
 
 void setup() {
     Serial.begin(74880);
-    while (!Serial)
-        ;
     for (int i = 0; i < buttons_count; i++) {
-        if (button_to_gpio[i] != 0xFF) {
+        if (button_to_gpio[i] != UNUSED) {
             pinMode(button_to_gpio[i], OUTPUT);
+            digitalWrite(button_to_gpio[i], LOW);
         }
     }
     IrReceiver.begin(IR_PIN, ENABLE_LED_FEEDBACK);
-    Serial.println("IR Receiver initialized. Waiting for codes...");
+    Serial.println(F("IR Receiver initialized. Waiting for codes..."));
 }
 
 static constexpr uint32_t RELEASE_TIMEOUT = 200;
@@ -64,20 +54,22 @@ static constexpr uint32_t RELEASE_TIMEOUT = 200;
 void loop() {
     static bool keyIsDown = false;
     static uint32_t lastRecvTime = 0;
-    static uint32_t lastCommand = 0;
-    if (IrReceiver.decode() && IrReceiver.decodedIRData.protocol == SAMSUNG) {
+    if (IrReceiver.decode()) {
+        if (IrReceiver.decodedIRData.protocol != SAMSUNG || IrReceiver.decodedIRData.command >= buttons_count) {
+            IrReceiver.resume();
+            return;
+        }
         if (!keyIsDown) {
             keyIsDown = true;
-            switch_buttons(1 << (IrReceiver.decodedIRData.command & 0xF));
+            switch_buttons(1 << IrReceiver.decodedIRData.command);
             Serial.print(F("Key DOWN: 0x"));
-            Serial.println(lastCommand, HEX);
+            Serial.println(IrReceiver.decodedIRData.command, HEX);
         }
         lastRecvTime = millis();
+        IrReceiver.resume();
     } else if (keyIsDown && (millis() - lastRecvTime > RELEASE_TIMEOUT)) {
         keyIsDown = false;
         Serial.println(F("All keys UP"));
         switch_buttons(0);
     }
-
-    IrReceiver.resume();
 }
